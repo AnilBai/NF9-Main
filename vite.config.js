499987d165cdd22c9a01a45245335afed3c6ff3d
Anil Bai <anilbai397@gmail.com>
2026-03-29 16:29:30 +0530

    updated pages

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     dedupe: ['react', 'react-dom'],
   },
 })
-
-// deploy test - 19 march 2026
